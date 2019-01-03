df19893ca297e8892f22221006faaf1352a27953
Laurenz Vercalsteren <laurenz97@hotmail.com>
2019-01-03 18:16:00 +0100

    fix

--- app/src/main/java/be/thomasmore/myfonoapp/SpelFragment.java
+++ app/src/main/java/be/thomasmore/myfonoapp/SpelFragment.java
@@ -539,25 +539,25 @@ public class SpelFragment extends Fragment {
         int keuzeId = pref.getInt("keuze",0);
 
         switch (keuzeId) {
-            case 2131230893: list = listGSV;
+            case 2131230891: list = listGSV;
                 break;
-            case 2131230891: list = listGK;
+            case 2131230889: list = listGK;
                 break;
-            case 2131230895: list = listKTI;
+            case 2131230893: list = listKTI;
                 break;
-            case 2131230894: list = listKTF;
+            case 2131230892: list = listKTF;
                 break;
-            case 2131230892: list = listGS;
+            case 2131230890: list = listGS;
                 break;
-            case 2131230896: list = listNGN;
+            case 2131230894: list = listNGN;
                 break;
-            case 2131230890: list = listFT;
+            case 2131230888: list = listFT;
                 break;
-            case 2131230897: list = listST;
+            case 2131230895: list = listST;
                 break;
-            case 2131230889: list = listCHT;
+            case 2131230887: list = listCHT;
                 break;
-            case 2131230898: list = listSZT;
+            case 2131230896: list = listSZT;
                 break;
             default: list = listSZT;
                 break;
